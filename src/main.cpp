42faa9f7106c7796ef503f6f5494e0ee7f849231
Remi Caput <git@mirari.fr>
2013-05-16 00:24:30 +0200

    Add simple loading bar, update Glay.

--- src/main.cpp
+++ src/main.cpp
@@ -11,32 +11,69 @@ using namespace Glay;
 using namespace Glay::Pipe;
 using namespace Tesca;
 
-int	execute (const Provision::Input& input, const Provision::Lookup& lookup, const Expression::Filter& filter, Expression::Calculator& calculator, const Render::Output& output, char* sources[], int length)
+void	initialize (Arithmetic::Table& table, const Expression::Filter& filter, Expression::Calculator& calculator)
 {
-	Render::Printer*	printer;
+	table.reset (filter.getCondition (), calculator.getColumns (), calculator.getSlots ());
+}
+
+void	loading (FormatWriter& writer, const char* caption, const Provision::Reader::Progress& progress)
+{
+	static const Int32u	captionWidth = 30;
+	static const Int32u	meterWidth = 25;
+
+	Int32u	i;
+	Int32u	u;
+
+	writer.write ('\r');
+
+	for (i = 0; i < captionWidth && *caption != '\0'; ++i)
+		writer.write (*caption++);
+
+	for (; i < captionWidth; ++i)
+		writer.write (' ');
+
+	writer
+		.write (" [");
+
+	u = progress.read * meterWidth / progress.size;
+
+	for (i = 0; i < u; ++i)
+		writer.write ('#');
+
+	for (i = u; i < meterWidth; ++i)
+		writer.write ('-');
+
+	writer
+		.write ("] ")
+		.write (progress.read * 100 / progress.size)
+		.write ('%')
+		.flush ();
+}
+
+void	process (Arithmetic::Table& table, const Provision::Input& input, const Provision::Lookup& lookup, bool progress, char* sources[], int length)
+{
+	const char*			caption;
 	Provision::Reader*	reader;
 	FileIStream			source;
-	Arithmetic::Table	table;
-	FileOStream			target (stdout);
+	Int8u				status;
 	FormatWriter		writer (err);
 
-	printer = output.create ();
-
-	if (!printer)
-	{
-		writer.write ("error: cannot create printer\n");
-
-		return 1;
-	}
-
-	table.reset (filter.getCondition (), calculator.getColumns (), calculator.getSlots ());
+	status = progress ? 1 : 0;
 
 	for (int index = 0; index < length || index == 0; ++index)
 	{
 		if (index < length)
+		{
 			source.open (sources[index]);
+
+			caption = sources[index];
+		}
 		else
+		{
 			source.open (stdin);
+
+			caption = "<standard input>";
+		}
 
 		if (source)
 		{
@@ -46,41 +83,49 @@ int	execute (const Provision::Input& input, const Provision::Lookup& lookup, con
 			{
 				reader->onError ().bind ([&] (const string& error)
 				{
+					if (status > 1)
+						writer.write ("\n");
+
 					writer
 						.write ("warning: reader error(")
 						.write (error)
 						.write(")\n");
 				});
 
-				reader->onRead ().bind ([&] (const Provision::Reader::Progress&)
+				
+				reader->onRead ().bind ([&] (const Provision::Reader::Progress& progress)
 				{
-					// FIXME
+					if (status > 0 && progress.size > 0)
+					{
+						loading (writer, caption, progress);
+
+						status = 2;
+					}
 				});
 
 				while (reader->next ())
 					table.push (reader->current ());
+
+				if (status > 1)
+				{
+					writer
+						.write ("\n")
+						.flush ();
+				}
 
 				delete reader;
 			}
 			else
 				writer.write ("error: cannot create reader\n");
 		}
-		else if (index < length)
+		else
 		{
 			writer
-				.write ("error: cannot open file '")
-				.write (sources[index])
+				.write ("error: cannot open '")
+				.write (caption)
 				.write ("' for reading\n");
 		}
-		else
-			writer.write ("error: cannot open standard input for reading");
 	}
-
-	printer->print (target, table);
-
-	delete printer;
-
-	return 0;
 }
 
 int	main (int argc, char* argv[])
@@ -95,7 +140,10 @@ int	main (int argc, char* argv[])
 	Provision::Lookup		lookup;
 	Render::Output			output;
 	const char*				outputFormat;
+	Render::Printer*		printer;
+	bool					progress;
 	int						start;
+	Arithmetic::Table		table;
 	FormatWriter			writer (err);
 
 	calculator.onError ().bind ([&] (const string& message)
@@ -134,6 +182,7 @@ int	main (int argc, char* argv[])
 	filterCondition = "true";
 	inputFormat = "csv";
 	outputFormat = "pretty";
+	progress = false;
 
 	for (start = 1; start < argc && argv[start][0] == '-'; ++start)
 	{
@@ -173,7 +222,8 @@ int	main (int argc, char* argv[])
 						.write ("  -c: filter condition, e.g. 'len(#0) >= 3'\n")
 						.write ("  -e: calculator expression, e.g. '#0: name, sum(#1): duration'\n")
 						.write ("  -i: input stream format, e.g. 'csv'\n")
-						.write ("  -o: output stream format, e.g. 'csv'\n");
+						.write ("  -o: output stream format, e.g. 'csv'\n")
+						.write ("  -p: display progress bar\n");
 
 					return 0;
 
@@ -201,6 +251,11 @@ int	main (int argc, char* argv[])
 
 					break;
 
+				case 'p':
+					progress = true;
+
+					break;
+
 				default:
 					writer
 						.write ("error: unknown option '")
@@ -218,5 +273,21 @@ int	main (int argc, char* argv[])
 	    !output.parse (outputFormat))
 		return 1;
 
-	return execute (input, lookup, filter, calculator, output, argv + start, argc - start);
+	printer = output.create ();
+
+	if (!printer)
+	{
+		writer.write ("error: cannot create printer\n");
+
+		return 1;
+	}
+
+	initialize (table, filter, calculator);
+	process (table, input, lookup, progress, argv + start, argc - start);
+
+	printer->print (out, table);
+
+	delete printer;
+
+	return 0;
 }
